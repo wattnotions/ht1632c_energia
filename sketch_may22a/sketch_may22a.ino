6052adbbe3c0998a9e20ce5d06f2bd8ff5d757ae
wattnotions <shaneormonde@gmail.com>
2015-05-22 09:48:16 -0700

    test

---
+++ sketch_may22a/sketch_may22a.ino
@@ -0,0 +1,40 @@
+#define DATA_PIN   P2_2
+#define WR         P2_1
+#define CS         P2_0
+#define RD         P2_3
+#define MSB_FIRST  0
+#define LSB_FIRST  1
+
+void setup()
+{
+  // put your setup code here, to run once:
+  
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  
+}
+
+
+void ht1632Write(uint8_t  mem_addr, uint8_t data) {
+  
+  
+}
+
+void clockPulse{
+  
+  digitalWrite(WR, HIGH);
+  digitalWrite(WR, LOW);
+  
+}
+
+void writeBits {uint8_t bits, uint8_t num_bits , uint8_t bit_order) {
+   
+    
+}
+  
+  
+  
+  
